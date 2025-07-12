95fc3c216d2478b228ac3e90d7aecf87679d8f25
xinhuey <xinhuey.wong@gmail.com>
2025-07-11 22:42:47 -0400

    added scrollview, need to reformat the image upload section

--- app/src/main/java/com/example/learntocook/CreateBlogPostActivity.kt
+++ app/src/main/java/com/example/learntocook/CreateBlogPostActivity.kt
@@ -3,12 +3,23 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import androidx.core.app.ActivityCompat
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.learntocook.databinding.ActivityCreateBlogPostBinding
 
 class CreateBlogPostActivity : AppCompatActivity() {
     private lateinit var binding : ActivityCreateBlogPostBinding
 
+    private var selectedImageUri: Uri?= null
+
+    private val imagePickerLauncher = registerForActivityResult(ActivityResultContracts.GetContent()){
+        uri: Uri? ->
+        uri?.let{
+            selectedImageUri = it
+            binding.imagePreview.setImageURI(it)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         binding = ActivityCreateBlogPostBinding.inflate(layoutInflater)
@@ -18,6 +29,10 @@ class CreateBlogPostActivity : AppCompatActivity() {
             finish()
         }
 
+        binding.imagePreview.setOnClickListener{
+            imagePickerLauncher.launch("image/*")
+        }
+
         binding.buttonPost.setOnClickListener{
             val title = binding.editTitle.text.toString().trim()
             val description = binding.editDescription.text.toString().trim()
@@ -25,7 +40,7 @@ class CreateBlogPostActivity : AppCompatActivity() {
             val instructions = binding.editInstructions.text.toString().trim()
 
             if (title.isEmpty() || description.isEmpty() || ingredients.isEmpty()
-                || instructions.isEmpty()){
+                || instructions.isEmpty() ||selectedImageUri == null){
 
                 Toast.makeText(this, "Please fill out all fields", Toast.LENGTH_SHORT).show()
 
